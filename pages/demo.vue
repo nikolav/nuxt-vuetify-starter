2bb181a2855f24489411425dc2f9063c70da2e56
Vukovic Nikola <admin@nikolav.rs>
2024-09-01 19:35:33 +0200

    Sun Sep  1 17:35:33 UTC 2024

--- pages/demo.vue
+++ pages/demo.vue
@@ -4,15 +4,12 @@ import { Dump } from "@/components/dev";
 //   layout: false,
 // });
 
-const { store, rate } = useTopicRating("$_1");
-const range16 = range(1, 6);
 // @@eos
 </script>
 <template>
   <section class="page--demo">
-    <VBtn v-for="i in range16" :key="i" @click="rate(i)">r:{{ i }}</VBtn>
     <VResponsive max-width="550" class="mx-auto overflow-visible">
-      <Dump :data="{ store }" />
+      122333
     </VResponsive>
   </section>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,15 @@
 <script setup lang="ts">
 import { Dump } from "@/components/dev";
-import { ProvideTranslation } from "@/components/lang";
-import {
-  langCodeEn,
-  langCodeSrLatn,
-  langCodeSrCyr,
-} from "@/config/i18n.lang-codes";
-
-//
-const { n, d, t, locale } = useI18n();
-const setLocale_ = useI18NSetLocale();
-
-const date = new Date();
-const auth = useStoreApiAuth();
-const authAdmin = async () =>
-  await auth.login({
-    email: "admin@nikolav.rs",
-    password: "5ba8de29-93bb-5bc2-9e03-b1a8c7c6737b",
-  });
-
-const { data: dataApiStatus } = useFetchApiStatus();
-const { status: graphqlStatus } = useQueryGraphqlStatus();
 
 // @@eos
 </script>
 <template>
   <section class="page--index">
-    <VCard max-width="640" class="mx-auto">
-      <VCardActions>
-        <VBtn color="primary" @click="setLocale_(langCodeSrLatn)"
-          >sr-Latn-RS</VBtn
-        >
-        <VBtn color="primary" @click="setLocale_(langCodeSrCyr)"
-          >sr-Cyrl-RS</VBtn
-        >
-        <VBtn @click="setLocale_(langCodeEn)">en</VBtn>
-      </VCardActions>
-      <VCardText>
-        <pre>{{ locale }}</pre>
-        <pre>{{ t("message.homeAddress") }}</pre>
-        <p>
-          {{ t("message.welcome") }} | {{ d(date, "long") }} |
-          {{ n(122333, "currency") }}
-        </p>
-        <ProvideTranslation
-          :query="{
-            q: 'Ne laissez pas le bruit des opinions des autres étouffer votre propre voix intérieure. Ayez le courage de suivre votre cœur et votre intuition.',
-            target: locale,
-          }"
-          v-slot="{ translation }"
-        >
-          <small>--</small>
-          <p>{{ translation }}</p>
-          <small>--</small>
-        </ProvideTranslation>
-        <Dump
-          :data="{
-            user: auth.user$,
-            dataApiStatus,
-            graphqlStatus,
-          }"
-        />
-      </VCardText>
-      <VCardActions>
-        <VSpacer />
-        <VBtn @click="authAdmin">login</VBtn>
-        <VBtn @click="auth.logout()">logout</VBtn>
-      </VCardActions>
-    </VCard>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit id
+      laudantium quo eaque nulla maxime optio, consequuntur, sed laborum impedit
+      ab suscipit dolor harum corporis nobis quia incidunt accusantium officiis.
+    </p>
   </section>
 </template>
 <style lang="scss" scoped></style>
