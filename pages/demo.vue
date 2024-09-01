a2bbd22a03080c7d671f6eee72cabbed6245a772
Vukovic Nikola <admin@nikolav.rs>
2024-09-01 19:18:54 +0200

    Sun Sep  1 17:18:54 UTC 2024

--- pages/demo.vue
+++ pages/demo.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 import { Dump } from "@/components/dev";
-import { renderIcon } from "@/components/icons";
 // definePageMeta({
 //   layout: false,
 // });
 
 const { store, rate } = useTopicRating("$_1");
+const range16 = range(1, 6);
 // @@eos
 </script>
 <template>
   <section class="page--demo">
-    <VBtn v-for="i in range(1, 6)" @click="rate(i)" :key="i">r:{{ i }}</VBtn>
+    <VBtn v-for="i in range16" :key="i" @click="rate(i)">r:{{ i }}</VBtn>
     <VResponsive max-width="550" class="mx-auto overflow-visible">
       <Dump :data="{ store }" />
     </VResponsive>
